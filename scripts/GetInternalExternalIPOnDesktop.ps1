0d1b7b94b1807618d28509ab5d92efd3bb84c115
Madars Šmits <Madars.Smits@outlook.lv>
2018-09-18 15:14:27 +0300

    Trying CustomScript

---
+++ scripts/GetInternalExternalIPOnDesktop.ps1
@@ -0,0 +1,8 @@
+﻿$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+$getInternalIP = Get-NetIPAddress | Format-Table
+$getExternalIP = Invoke-RestMethod http://ipinfo.io/json
+$datetime = Get-Date
+$resultsExternal = "ExternalIP info: $getExternalIP`n Date and time: $datetime"
+$resultsExternal | Out-File "$DesktopPath\ComputerExternalIPAddress.csv"
+$resultsInternal = $getInternalIP + "Date and time: $datetime"
+$resultsInternal | Out-File "$DesktopPath\ComputerInternalIPAddress.csv"
